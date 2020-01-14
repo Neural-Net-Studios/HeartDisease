ade01ed2f28d4cac8540a1c8ad30240bf90ad470
Matvei <57399129+Neenizheb@users.noreply.github.com>
2020-01-14 20:45:41 +0300

    Update NeuralNetwork.py

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -2,7 +2,7 @@ from sklearn import tree
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-data = pd.read_csv('C:/Users/asynk/Desktop/Data/heart.csv')
+data = pd.read_csv('PATH')
 x = data.drop(['target'], axis = 1)
 y = data['target']
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.33, random_state = 100)
