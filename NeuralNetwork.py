4f4a7df27e5bb0dd2167648498145f1bda014490
Matvei <57399129+Neenizheb@users.noreply.github.com>
2020-01-14 20:53:53 +0300

    Update NeuralNetwork.py

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -5,7 +5,7 @@ from sklearn.metrics import accuracy_score
 data = pd.read_csv('PATH')
 x = data.drop(['target'], axis = 1)
 y = data['target']
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.33, random_state = 100)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.33)
 clf = tree.DecisionTreeClassifier(max_depth=3)
 clf.fit(x_train, y_train)
 val_acc = accuracy_score(y_test, clf.predict(x_test))
